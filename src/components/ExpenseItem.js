6ae65fbfd761f244905d1f95ea29867df917eb5e
kunduaritra <kunduaritra7@gmail.com>
2023-04-14 20:27:15 +0530

    Variable added in the JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
 import './ExpenseItem.css'
 function ExpenseItem (){
+    const expenseDate = new Date(2023,4,14)
+    const expenseTitle = "Car Insurence"
+    const expenseAmount = 5000
+    const locationOfExpenditure = 'Acko Car Insurence'
     return (
         <div className='expense-item'>
-            <div>April 14th 2023</div>
+            <div>{expenseDate.toISOString}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurence</h2>
-                <div className='expense-item__price'>₹5000</div>
+                <h2>{expenseTitle} : Place: {locationOfExpenditure}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
             </div>
     </div>
     );
