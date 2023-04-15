46f9f2d31c29423555a00e2569a5a4f031bb0947
kunduaritra <kunduaritra7@gmail.com>
2023-04-15 20:45:10 +0530

    Expense items added in App.js and passed it to ExpenseItem.js props also used loops to pass those values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
 import './ExpenseItem.css'
-function ExpenseItem (){
-    const expenseDate = new Date(2023,4,14)
-    const expenseTitle = "Car Insurence"
-    const expenseAmount = 5000
-    const locationOfExpenditure = 'Acko Car Insurence'
+function ExpenseItem (props){
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle} : Place: {locationOfExpenditure}</h2>
-                <div className='expense-item__price'>{expenseAmount}</div>
+                <h2>{props.title} : Place: {props.location}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
     </div>
     );
