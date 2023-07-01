fa967378590233825bd71ec23caf0d38247871e1
kunduaritra <kunduaritra7@gmail.com>
2023-07-01 12:37:49 +0530

    Conditional Rendering showing Add New Expense form by clicking button also added cancel button to hide the form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,80 +2,92 @@ import React, { useState } from "react";
 import './ExpenseForm.css'
 
 const ExpenseFrom = (props) => {
-    const [enteredtitle, setEnteredTitle] = useState('')
-    const [enteredAmount , setEnteredAmount ] = useState('')
-    const [enteredDate  , setEnteredDate ] = useState('')
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enterAmount: '',
-    //     enterDate: ''
-    // })
-    const titleChangeHandler = (event) =>{
-        // console.log('changed title')
-        setEnteredTitle(event.target.value)
-        // Method 2
-        // setUserInput({
-        //     ...userInput,
-        //     enteredTitle: event.target.value,
-        // })
-        // method 3
-        // setUserInput((prevState)=>{
-        //     return {...prevState, enteredTitle: event.target.value}
-        // })
-    }
-    const amountChangeHandler = (event) =>{
-        // console.log('changed amount')
-        setEnteredAmount(event.target.value)
-        // setUserInput({
-        //     ...userInput,
-        //     enterAmount: event.target.value,
-        // })
-    }
-    const dateChangeHandler = (event) =>{
-        // console.log('changed date')
-        setEnteredDate(event.target.value)
-        // setUserInput({
-        //     ...userInput,
-        //     enterDate: event.target.value,
-        // })
-    }
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const [isFormVisible, setIsFormVisible] = useState(false);
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
     const addExpense = (event) => {
         event.preventDefault();
         const expenseData = {
-            title: enteredtitle,
+            title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate),
-        }
+        };
         props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
+        setIsFormVisible(false);
+    };
+    const showFormHandler = () =>{
+        setIsFormVisible(true);
     }
-    return(
-        <form onSubmit={addExpense}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label htmlFor="title">Title:</label>
-                    <input 
-                    type="text" 
-                    id="title" 
-                    value={enteredtitle} 
-                    onChange={titleChangeHandler} />
+    const hideFormHandler = () => {
+        setIsFormVisible(false);
+    }
+
+    let expenseContent = <button onClick={showFormHandler}>Add New Expense</button>;
+
+    if(isFormVisible) {
+        expenseContent = 
+            <form onSubmit={addExpense}>
+                <div className="new-expense__controls">
+                    <div className="new-expense__control">
+                        <label htmlFor="title">Title:</label>
+                        <input 
+                            type="text" 
+                            id="title" 
+                            value={enteredTitle} 
+                            onChange={titleChangeHandler} 
+                        />
+                    </div>
+                    <div className="new-expense__control">
+                        <label htmlFor="amount">Amount:</label>
+                        <input 
+                            type="number" 
+                            id="amount" 
+                            min="0.01" 
+                            step="0.01" 
+                            value={enteredAmount} 
+                            onChange={amountChangeHandler} 
+                        />
+                    </div>
+                    <div className="new-expense__control">
+                        <label htmlFor="date">Date:</label>
+                        <input 
+                            type="date" 
+                            id="date" 
+                            min="2019-01-01" 
+                            max="2023-06-28" 
+                            value={enteredDate} 
+                            onChange={dateChangeHandler} 
+                        />
+                    </div>
                 </div>
-                <div className="new-expense__control">
-                    <label htmlFor="amount">Amount:</label>
-                    <input type="number" id="amount" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
+                <div className="new-expense__actions">
+                    <button onClick={hideFormHandler}>Cancel</button><button type="submit">Add Expense</button>
                 </div>
-                <div className="new-expense__control">
-                    <label htmlFor="title">Date:</label>
-                    <input type="date" id="date" min="2019-01-01" max="2023-06-28" value={enteredDate} onChange={dateChangeHandler} />
-                </div>
-                </div>
-            <div className="new-expense__actions">
-                <button type="submit" >Add Expense</button>
-            </div>
-        </form>
-    )
-}
+            </form>
+    };
+
+    return (
+        <div>
+            {expenseContent}
+        </div>
+    );
+};
 
 export default ExpenseFrom;
